ecffeb24a725f227df618e3e036bcc8de30d5bad
Anja Kempf <kempfa@in.tum.de>
2020-01-02 17:29:15 +0100

    Unity UI and Asset for Kinect, maybe you have to download it on your pc again

---
+++ ScanUnity/Assets/Scripts/UIGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGame : MonoBehaviour
+{
+    public void endGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void configKinect()
+    {
+        
+    }
+}
